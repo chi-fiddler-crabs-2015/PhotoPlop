fc8584686b963b772a3dad9ddb4eb05c8a713961
Kara A Carrell <karaAJC@gmail.com>
2015-03-26 14:12:46 -0500

    adding omniauth columns to user migration and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook, :twitter, :instagram]
 end
