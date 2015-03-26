506628b50835a0b0c60d9aad2960dac76ba18406
Ty Kowalewski <tykowale@gmail.com>
2015-03-26 15:20:36 -0500

    Fix all specs and migrations for models

--- spec/models/albums_spec.rb
+++ spec/models/albums_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Album, type: :model do
 
   it { should have_many(:images) }
 
-  it { should belong_to(:user) }
+  it { should belong_to(:creator) }
 
-  it { should validate_uniqueness_of(:vanity_url) }
+  it { should validate_presence_of(:title) }
+
+  it { should validate_presence_of(:vanity_url) }
 end
