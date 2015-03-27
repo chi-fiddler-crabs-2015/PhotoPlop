bf795f4dd4fd2c6401cb3073578a716b7e2a2225
Kara A Carrell <karaAJC@gmail.com>
2015-03-27 10:28:07 -0500

    adding imagefile uploader to image model with cloudinary uploader

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
   belongs_to :album
-
+  mount_uploader :image, ImagefileUploader
   validates :url, presence: true
 
   # we get url from uploading the image after the instance of the model is created
