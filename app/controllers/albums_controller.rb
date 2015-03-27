be9564fcb4c7d4f72c89a759ccbd6e7c36b4ee6d
Stephanie Marx <stephanie.n.marx@gmail.com>
2015-03-27 11:37:25 -0500

    Fix typo in album_params

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,8 @@
 class AlbumsController < ApplicationController
 
   def index
+    @owned_albums = current_user.albums
+    @contributed_albums = current_user.collaborators_albums.albums
   end
 
   def new
@@ -8,21 +10,23 @@ class AlbumsController < ApplicationController
   end
 
   def create
-    @album = Album.new(album_params)
-
-    if @album.save
+    new_album = current_user.albums.create(album_params)
+    if new_album.valid?
+      redirect_to album_path(new_album)
     else
+      @errors = new_album.errors
+      render :'new'
     end
-
   end
 
   def show
     @album = Album.find_by(id: params[:id])
   end
+
   private
 
   def album_params
-    params.require(:album).permit(:title, :description, :vanitiy_url, :password)
+    params.require(:album).permit(:title, :description, :vanity_url, :password)
   end
 
 end
