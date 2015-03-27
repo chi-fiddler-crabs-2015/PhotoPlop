6fe4fbd60156037ee8f7aa760fef05f411bb71eb
Kara A Carrell <karaAJC@gmail.com>
2015-03-27 11:32:40 -0500

    updating gitignore, commenting out omniauth functionality for mvp and adding devise test helper configs to help pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook, :twitter, :instagram]
+         :recoverable, :rememberable, :trackable, :validatable
+         # :omniauthable, :omniauth_providers => [:facebook, :twitter, :instagram]
+
+  has_many :albums, foreign_key: :creator_id
+  has_many :collaborators_albums, foreign_key: :collaborator_id
+  has_many :images
+
 end
